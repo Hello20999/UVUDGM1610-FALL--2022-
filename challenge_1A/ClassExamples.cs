3fda7596e7f338c0714f830a6632878dd10fa904
Hello20999 <matthew.hello2099@gmail.com>
2022-09-09 22:22:03 -0400

    added all function, need to be edited

---
+++ challenge_1A/ClassExamples.cs
@@ -0,0 +1,22 @@
+//class examples
+public class ClassExample 
+{
+    
+}
+
+public class ClassWithVariables
+{
+    public int number =2;
+    public string userName = "Bob";
+}
+
+public class ClassWithFunctions
+{
+    public void RunFuction()
+    {
+        Console.WriteLine("Running Function");
+    }
+}
+
+
+//need to add more funtion to the classwithcariables and class with functions
--- challenge_1A/HelloWorld.cs
+++ challenge_1A/HelloWorld.cs
@@ -3,7 +3,6 @@ using System;
 
 public class HelloWorld
 {
-    private int number = 2;
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello World");
