4ecff81579ff1afe96bddb0b0dca8ddf4accef8e
Hello20999 <matthew.hello2099@gmail.com>
2022-09-10 20:58:48 -0400

    added Fortnite

--- challenge_1A/MyClass.cs
+++ challenge_1A/MyClass.cs
@@ -45,6 +45,7 @@ public class HowToGetAChickenDinner
         Sneaky.winnerWinnerChickenDinner = 9;
 
        Console.WriteLine("Pubg kill/death and win/death ratio");
+       Console.WriteLine(" ");
         Console.WriteLine("Camping in the corner strat");
         Console.WriteLine("kill/death ratio, "+ campInCorner.kills + "/"+ campInCorner.deaths);
         Console.WriteLine("Win/lose ratio, "+ campInCorner.winnerWinnerChickenDinner + "/" + campInCorner.deaths);
@@ -68,11 +69,52 @@ public class HowToGetAChickenDinner
         campInOneWayRoom = new FortNight();
         sneaky = new FortNight();
 
+        bushcamp.deaths = 40;
+        bushcamp.kills = 9;
+        bushcamp.victoryRoyal = 0;
+
+        constantRunning.deaths = 90;
+        constantRunning.kills = 34;
+        constantRunning.victoryRoyal = 13;
+
+        campInOneWayRoom.deaths = 30;
+        campInOneWayRoom.kills = 20;
+        campInOneWayRoom.victoryRoyal = 3;
+
+        sneaky.deaths = 40;
+        sneaky.kills = 25;
+        sneaky.victoryRoyal = 3;
+
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+        Console.WriteLine("Fortnight kill/death and win/death ratio");
+        Console.WriteLine(" ");
+        Console.WriteLine("Camping in the corner strat");
+        Console.WriteLine("kill/death ratio, "+ campInOneWayRoom.kills + "/"+ campInOneWayRoom.deaths);
+        Console.WriteLine("Win/lose ratio, "+ campInOneWayRoom.victoryRoyal + "/" + campInOneWayRoom.deaths);
+        Console.WriteLine(" ");
+        Console.WriteLine("Playing aggressivly");
+        Console.WriteLine("kill/death ratio, "+ constantRunning.kills + "/"+ constantRunning.deaths);
+        Console.WriteLine("Win/lose ratio, "+ constantRunning.victoryRoyal  + "/" + constantRunning.deaths);
+        Console.WriteLine(" ");
+        Console.WriteLine("Bush camp the entire game");
+        Console.WriteLine("kill/death ratio, "+ bushcamp.kills + "/"+ bushcamp.deaths);
+        Console.WriteLine("Win/lose ratio, "+  bushcamp.victoryRoyal + "/" + bushcamp.deaths);
+        Console.WriteLine(" ");
+        Console.WriteLine("Playing carfully");
+        Console.WriteLine("kill/death ratio, "+ sneaky.kills + "/"+ sneaky.deaths);
+        Console.WriteLine("Win/lose ratio, "+ sneaky.victoryRoyal  + "/" + sneaky.deaths);
+        Console.WriteLine(" ");
+
         //population one
         houseCamp = new PopulationOne();
         areaLooter = new PopulationOne();
         climeTowerCamp = new PopulationOne();
         Hunting = new PopulationOne();
+
+        houseCamp.deaths = 40;
+        houseCamp.kills = 9;
+        houseCamp.victory = 0;
     }
 }
 public class Pubg
