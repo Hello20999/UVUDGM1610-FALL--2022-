17051bf8d51ed592ef307f21ac3318ec55bad89a
Hello20999 <matthew.hello2099@gmail.com>
2022-09-10 20:48:59 -0400

    pubg added

--- challenge_1A/MyClass.cs
+++ challenge_1A/MyClass.cs
@@ -14,6 +14,12 @@ public class HowToGetAChickenDinner
     public FortNight constantRunning;
     public FortNight campInOneWayRoom;
     public FortNight sneaky;
+
+    //Population one
+    public PopulationOne houseCamp;
+    public PopulationOne areaLooter;
+    public PopulationOne climeTowerCamp;
+    public PopulationOne Hunting;
     public void Main()
     {
         //pubg
@@ -22,12 +28,51 @@ public class HowToGetAChickenDinner
         lowCrawling = new Pubg ();
         Sneaky = new Pubg ();
 
+        campInCorner.deaths = 10;
+        campInCorner.kills = 4;
+        campInCorner.winnerWinnerChickenDinner = 0;
+
+        aggressive.deaths = 43;
+        aggressive.kills = 23;
+        aggressive.winnerWinnerChickenDinner = 6;
+
+        lowCrawling.deaths = 62;
+        lowCrawling.kills = 13;
+        lowCrawling.winnerWinnerChickenDinner = 1;
+
+        Sneaky.deaths = 32;
+        Sneaky.kills = 20;
+        Sneaky.winnerWinnerChickenDinner = 9;
+
+       Console.WriteLine("Pubg kill/death and win/death ratio");
+        Console.WriteLine("Camping in the corner strat");
+        Console.WriteLine("kill/death ratio, "+ campInCorner.kills + "/"+ campInCorner.deaths);
+        Console.WriteLine("Win/lose ratio, "+ campInCorner.winnerWinnerChickenDinner + "/" + campInCorner.deaths);
+        Console.WriteLine(" ");
+        Console.WriteLine("Playing aggressivly");
+        Console.WriteLine("kill/death ratio, "+ aggressive.kills + "/"+ aggressive.deaths);
+        Console.WriteLine("Win/lose ratio, "+ aggressive.winnerWinnerChickenDinner  + "/" + aggressive.deaths);
+        Console.WriteLine(" ");
+        Console.WriteLine("Lowcrawling the entire game");
+        Console.WriteLine("kill/death ratio, "+ lowCrawling.kills + "/"+ lowCrawling.deaths);
+        Console.WriteLine("Win/lose ratio, "+ lowCrawling.winnerWinnerChickenDinner + "/" + lowCrawling.deaths);
+        Console.WriteLine(" ");
+        Console.WriteLine("Playing carfully");
+        Console.WriteLine("kill/death ratio, "+ Sneaky.kills + "/"+ Sneaky.deaths);
+        Console.WriteLine("Win/lose ratio, "+ Sneaky.winnerWinnerChickenDinner  + "/" + Sneaky.deaths);
+        Console.WriteLine(" ");
+
         //fornight
         bushcamp = new FortNight();
         constantRunning = new FortNight();
         campInOneWayRoom = new FortNight();
         sneaky = new FortNight();
+
         //population one
+        houseCamp = new PopulationOne();
+        areaLooter = new PopulationOne();
+        climeTowerCamp = new PopulationOne();
+        Hunting = new PopulationOne();
     }
 }
 public class Pubg
@@ -35,21 +80,18 @@ public class Pubg
     public int deaths;
     public int kills;
     public int winnerWinnerChickenDinner;
-    public int ammount;
 }
 public class FortNight
 {
     public int deaths;
     public int kills;
-    public int winnerWinnerChickenDinner;
-    public int ammount;
+    public int victoryRoyal;
 }
-public class ApexLegends
+public class PopulationOne
 {
     public int deaths;
     public int kills;
-    public int winnerWinnerChickenDinner;
-    public int ammount;
+    public int victory;
 }
 
 
