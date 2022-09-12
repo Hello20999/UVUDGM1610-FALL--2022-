50655d4cd1a62b37fb54a86928d1e4eb01207122
Hello20999 <matthew.hello2099@gmail.com>
2022-09-12 19:27:02 -0400

    4th video

--- lab 1b/VoidPractice.cs
+++ lab 1b/VoidPractice.cs
@@ -8,10 +8,36 @@ public class VoidPractice
         DoMath(10, 4);
         DoMath(20, 7);
         DoMath(30, 15);
+        Compare(7,8);
+        Compare(8,7);
+        CheckPassword("no");
+        CheckPassword("why");
     }
     public void DoMath(int value, int value2)
     {
         var number = value + value2;
         Console.WriteLine(number);
+    }
+    public void Compare(int value, int value2)
+    {
+        if (value > value2)
+        {
+            Console.WriteLine("True, the first is greater.");
+        }
+        else
+        {
+            Console.WriteLine("False, the second is greater.");
+        }
+    }
+    public void CheckPassword(string password)
+    {
+        if (password == "why")
+        {
+            Console.WriteLine("correct password.");
+        }
+        else
+        {
+            Console.WriteLine("Incorrect password");
+        }
     }
 }
