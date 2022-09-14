0b1987abadf5b22bfa2dee97eece5632e577bdd9
Hello20999 <matthew.hello2099@gmail.com>
2022-09-14 18:19:45 -0400

    Then practice added

--- lab 1b/Practice1.cs
+++ lab 1b/Practice1.cs
@@ -4,16 +4,28 @@ public class VoidPractice1
 {
     public void Main()
     {
-        if (20 > 18) 
-{
-  Console.WriteLine("20 is greater than 18");
-  int x = 20;
-int y = 18;
-if (x > y) 
-{
-  Console.WriteLine("x is greater than y");
-}
-}
+       //if
+       if (20 > 18)    
+       {
+         Console.WriteLine("20 is greater than 18");
+       }
+         int x = 20;
+         int y = 18;
+         if (x > y) 
+         {
+            Console.WriteLine("x is greater than y");
+            }
+
+            //else
+            int time = 6;
+            if (time < 18) 
+            {
+                Console.WriteLine("Good day.");
+                } 
+                else 
+                {
+                    Console.WriteLine("Good evening.");
+        }
         
     }
 }
