14e8fb17049e655c129570e561212256cd53c05d
Hello20999 <matthew.hello2099@gmail.com>
2022-09-14 18:12:03 -0400

    IF practice

---
+++ lab 1b/Practice1.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class VoidPractice1
+{
+    public void Main()
+    {
+        if (20 > 18) 
+{
+  Console.WriteLine("20 is greater than 18");
+  int x = 20;
+int y = 18;
+if (x > y) 
+{
+  Console.WriteLine("x is greater than y");
+}
+}
+        
+    }
+}
