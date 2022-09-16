3452b2c9e69f421ad4c9f4e10be8dd50a612f879
Hello20999 <matthew.hello2099@gmail.com>
2022-09-16 19:28:14 -0400

    idea genurated, no clue how to make this as complicated as possible on purpose for no reason.

---
+++ lab 1b/Challenge1B.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class CookingFrenchToast
+{
+    public void Main()
+    {
+        Console.WriteLine("guess the equation");
+        // replace the number and the type of problem.  There are more than one right answer.
+        int first = 2;
+        int second = 3;
+        //do either "plus", "Minus", "Subtract", or "Devide
+        string type = "plus";
+        Console.WriteLine("current guess is " + first + type + second);
+    }
+}
+
+
+
+
+
+
+//dont look past here
