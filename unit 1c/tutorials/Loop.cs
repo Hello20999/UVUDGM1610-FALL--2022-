7c7a45e98556cdc9e072155bfec7b52afe2ce79b
Hello20999 <matthew.hello2099@gmail.com>
2022-09-19 21:09:53 -0400

    some looping practice

--- unit 1c/tutorials/Loop.cs
+++ unit 1c/tutorials/Loop.cs
@@ -3,4 +3,13 @@ using System;
 public class Looping
 {
     public void Main()
+    {
+        int i = 4;
+        do 
+        {
+            Console.WriteLine(i);
+            i--;
+        }
+        while (i < 21);
+    }
 }
