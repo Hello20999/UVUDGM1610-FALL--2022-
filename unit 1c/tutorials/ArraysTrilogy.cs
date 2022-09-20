08ea3e594aea478fdf68129713a1ddfe8ef387d7
Hello20999 <matthew.hello2099@gmail.com>
2022-09-19 21:58:14 -0400

    Renamed to make file locating easier, added example of choose one from list

---
+++ unit 1c/tutorials/ArraysTrilogy.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Arrays3
+{
+    public void Main()
+    {
+        //choosing one in a list
+        string[] planes = {"Delta", "Alaskan", "American"};
+        Console.WriteLine(planes[1]);
+    }
+}
