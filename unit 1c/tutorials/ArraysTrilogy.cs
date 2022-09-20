51264e6dd3717979a832aed3cf69a991c3ad21b1
Hello20999 <matthew.hello2099@gmail.com>
2022-09-19 22:02:39 -0400

    change a name in a list

--- unit 1c/tutorials/ArraysTrilogy.cs
+++ unit 1c/tutorials/ArraysTrilogy.cs
@@ -4,8 +4,13 @@ public class Arrays3
 {
     public void Main()
     {
-        //choosing one in a list
+        //choosing one in a list, the zero chooses the first listed item.
         string[] planes = {"Delta", "Alaskan", "American"};
-        Console.WriteLine(planes[1]);
+        Console.WriteLine(planes[0]);
+
+        //changing a name of an item in a list
+        string[] xbox = {"Xbox", "Xbox360", "Xbox one", "Xbox series"};
+        xbox[2] = "Playstations";
+        Console.WriteLine(xbox[2]);
     }
 }
