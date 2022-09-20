5824e99e78a6e88e7a608fbe7acb74ea13c0d0a2
Hello20999 <matthew.hello2099@gmail.com>
2022-09-19 20:17:47 -0400

    first video example

---
+++ unit 1c/tutorials/ArraysLooping.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class arrayVideo
+{
+    public void main()
+    {
+        string[] burger = {"burger", "Hamburger", "Cheeseburger", "Bacon burger"};
+        Console.WriteLine(burger.Length);
+        Console.WriteLine(burger [0]);
+        Console.WriteLine(burger [1]);
+        Console.WriteLine(burger [2]);
+        Console.WriteLine(burger [3]);
+    }
+}
