fba4b8297be48c55f40a54426f7e7f920767689d
Hello20999 <matthew.hello2099@gmail.com>
2022-09-19 22:18:54 -0400

    Before I delete it all again, here is a list and arrays with loop

--- unit 1c/tutorials/ArraysTrilogy.cs
+++ unit 1c/tutorials/ArraysTrilogy.cs
@@ -12,5 +12,16 @@ public class Arrays3
         string[] xbox = {"Xbox", "Xbox360", "Xbox one", "Xbox series"};
         xbox[2] = "Playstations";
         Console.WriteLine(xbox[2]);
+
+        //knowing a legth of a list in a 8 item list
+        string[] spaceGames = {"OuterWilds", "StarBound", "Deep Rock Galatic", "Halo", "Stellarus", "Universe Sand Box", "War Frame", "Destiny 2"};
+        Console.WriteLine(spaceGames.Length);
+
+        //Using a loop with a list
+        string[] minecraft = {"Steve", "Alex", "Enderman", "Zombie"};
+        for (int i = 0; i < minecraft.Length; i++) 
+        {
+            Console.WriteLine(minecraft[i]);
+        }
     }
 }
