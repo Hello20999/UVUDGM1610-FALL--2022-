67d81516a50cdc24459559d6765528faf1204200
Hello20999 <matthew.hello2099@gmail.com>
2022-09-19 21:46:06 -0400

    Adding more than just one

--- unit 1c/tutorials/LoopsTheSequal.cs
+++ unit 1c/tutorials/LoopsTheSequal.cs
@@ -4,9 +4,16 @@ public class Loops2
 {
     public void Main()
     {
+       //taking away 1 from 3 to a min of -2
        for (int i = 3; i > -2; i--) 
        {
         Console.WriteLine(i);
         } 
+
+        //adding value by 15 to max of 1000 from 100
+        for (int n = 100; n <= 1000; n = n + 15)
+        {
+            Console.WriteLine(n);
+        }
     }
 }
