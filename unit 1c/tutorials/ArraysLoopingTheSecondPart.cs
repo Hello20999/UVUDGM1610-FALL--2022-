c47a57ee24c550815e89f6809cf507e16fb8b867
Hello20999 <matthew.hello2099@gmail.com>
2022-09-19 21:02:26 -0400

    weapon class video

---
+++ unit 1c/tutorials/ArraysLoopingTheSecondPart.cs
@@ -0,0 +1,28 @@
+using System;
+
+public class TheSecondPart
+{
+    public static void main()
+    {
+        weapon[] weaponsobjs = {new weapon(), new weapon (), new weapon ()};
+        weaponsobjs[1].weaponName = "Great Axe";
+        weaponsobjs[1].weaponName = "Hatchet";
+
+
+        for (var i=0; i < weaponsobjs.Length; i++)
+        {
+            weaponsobjs[i].powerLevel = 2;
+        }
+        foreach (var item in weaponsobjs)
+        {
+            Console.WriteLine(item.weaponName);
+        }
+        Console.WriteLine(weaponsobjs[0].weaponName);
+
+    }
+}
+public class weapon
+{
+    public string weaponName = "axe";
+    public int powerLevel = 1;
+}
