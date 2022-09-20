cc5110806797b70153a4ed31fd9a3452aa8b7514
Hello20999 <matthew.hello2099@gmail.com>
2022-09-19 21:15:01 -0400

    Added other way to loop

--- unit 1c/tutorials/Loop.cs
+++ unit 1c/tutorials/Loop.cs
@@ -4,12 +4,21 @@ public class Looping
 {
     public void Main()
     {
+        // start at four and counts down to one and stops
         int i = 4;
         do 
         {
             Console.WriteLine(i);
             i--;
         }
-        while (i < 21);
+        while (i > 0);
+
+        //starts at zero, counts up to four and stops
+        int n = 0;
+        while (n < 5) 
+        {
+            Console.WriteLine(n);
+            n++;
+        }
     }
 }
