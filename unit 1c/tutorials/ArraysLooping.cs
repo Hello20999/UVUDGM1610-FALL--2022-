311e4379d6a3b8627a18808aa38853b3edd345d1
Hello20999 <matthew.hello2099@gmail.com>
2022-09-19 20:41:53 -0400

    more video and examples from them

--- unit 1c/tutorials/ArraysLooping.cs
+++ unit 1c/tutorials/ArraysLooping.cs
@@ -4,11 +4,35 @@ public class arrayVideo
 {
     public void main()
     {
+        //arrays
         string[] burger = {"burger", "Hamburger", "Cheeseburger", "Bacon burger"};
         Console.WriteLine(burger.Length);
         Console.WriteLine(burger [0]);
         Console.WriteLine(burger [1]);
         Console.WriteLine(burger [2]);
         Console.WriteLine(burger [3]);
+        foreach (var sandwhich in burger)
+        {
+            Console.WriteLine("How to eat a " + sandwhich);
+        }
+
+        //multiple
+        string[] pizza = {"cheese", "cheese", "cheese"};
+        for (var i = 0; i < pizza.Length; i++)
+        {
+            pizza[i] = "tomato";
+        }
+        foreach (var pizzas in pizza)
+        {
+            Console.WriteLine("Now the tables haved turned to " + pizzas);
+        }
+
+        //just one change
+        string[] lazer = {"cheese", "cheese", "cheese"};
+        lazer[1] = "tomato";
+        foreach (var lazers in lazer)
+        {
+            Console.WriteLine("Now the tables haved turned to " + lazers);
+        }
     }
 }
