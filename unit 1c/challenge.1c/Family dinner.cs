048b6408520ec3d71821bb04f671592192a77a6b
Hello20999 <matthew.hello2099@gmail.com>
2022-09-22 22:25:21 -0400

    Deleted previouse attempt, ready to try again

--- unit 1c/challenge.1c/Family dinner.cs
+++ unit 1c/challenge.1c/Family dinner.cs
@@ -1,15 +1,9 @@
 using System;
 
-public class NightOne
+public class FamilyDinner
 {
     public void Main()
     {
-        string[] people = {"Steve", "Sam", "Sanders", "Sofia", "Samuel", "Scarlet", "sophia"};
-        foreach (var names in people)
-        Console.WriteLine("It was a beutiful evening in the castle, where " + names + " fell asleep.");
-        Console.WriteLine("Most people thought it would be a nice night, but no one could of predicted the tragedy that would have occured.");
-        people[2] = "Sanders did not";
-        foreach (var steve in people)
-        Console.WriteLine("For when the sun arised," + steve + " awaken from their sleep");
+        
     }
 }
