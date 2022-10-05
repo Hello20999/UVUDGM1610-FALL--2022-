eba0a530f0e8616b1c914db9220d5eb83424b385
matthew <matthew.hello2099@gmail.com>
2022-10-04 22:06:50 -0400

    skipped reading for now, finished other scripts

---
+++ Unit3/Lab.3/My project/Assets/CheckStatus.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckStatus : MonoBehaviour
+{
+    // how to check if a script working in an object
+    void Start()
+    {
+        Debug.Log("Working");
+    }
+}
---
+++ Unit3/Lab.3/My project/Assets/PlayerName.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerName : MonoBehaviour
+{
+    public string myName;
+    void Start()
+    {
+        Debug.Log("I am alive and my name is " + myName);
+    }
+
+    //this is used to create an editable spot to input a name, in which will broadcast what you put.
+}
