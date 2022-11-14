f8a96fa80b56b02b83a4ff4c2df35a7adc58ab27
matthew <matthew.hello2099@gmail.com>
2022-11-14 10:40:18 -0500

    added sprite renderer behavior script

---
+++ Unit7/Matching/Assets/Lab7a/Scripts/SpriteBehavior.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteBehavior : MonoBehaviour
+{
+    private SpriteRenderer rendererObj;
+    private void Start()
+    {
+        rendererObj = GetComponent<SpriteRenderer>();
+    }
+}
