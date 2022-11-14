d226db20f84fc5267edfcaa2b9620499cf4e6357
matthew <matthew.hello2099@gmail.com>
2022-11-14 13:57:46 -0500

    High score added

--- Unit7/Matching/Assets/Lab7a/Scripts/intData.cs
+++ Unit7/Matching/Assets/Lab7a/Scripts/intData.cs
@@ -1,9 +1,29 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 [CreateAssetMenu]
 public class intData : ScriptableObject
 {
     public int value;
+    public void SetValue(int num)
+    {
+        value = num;
+    }
+    public void CompareValue(intData obj)
+    {
+        if (value >= obj.value)
+        {
+
+        }
+        else
+        {
+            value = obj.value;
+        }
+    }
+    public void SetValue(intData obj)
+    {
+        value = obj.value;
+    }
     public void UpdateValue(int num)
     {
         value += num;
